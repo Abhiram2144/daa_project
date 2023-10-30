a3bb73c5d48062c4e94472801226c642cd89fd8e
Abhi <96921292+Abhiram2144@users.noreply.github.com>
2023-10-30 09:26:21 +0530

    added the source code link

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <Layout>
       <div>
-        <a href="#" className='hover:underline'><h1>Source Code</h1></a>
+        <a href="https://github.com/Abhiram2144/daa_project" target='_blank' className='hover:underline'><h1>Source Code</h1></a>
       </div>
       <div className='flex items-center justify-center'>
         <h1>~Created by Abhiram & Ankur</h1>
